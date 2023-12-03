022da4ef72f912c6cee8e66a4b599f55ed6b1cb2
root <stefan.hadamek@fau.de>
2023-12-04 00:49:59 +0100

    Part 2/Day2

--- 02/solve02.py
+++ 02/solve02.py
@@ -11,23 +11,32 @@ for l in lines:
     iden = int(ids[5:])
     
     sets = cubes_org.split(";")
-    
+    curr_red,curr_green,curr_blue = 0,0,0
     impossible_game = False
     for sete in sets:
         print(sete)
-        red,green,blue = 0,0,0
+        
         cubes = sete.split(" ")
         for i in range(len(cubes)):
             if cubes[i].startswith("blue"):
                 blue = int(cubes[i-1])
+                if blue > curr_blue:
+                    curr_blue = blue
             elif cubes[i].startswith("red"):
                 red = int(cubes[i-1])
+                if red > curr_red:
+                    curr_red = red
             elif cubes[i].startswith("green"):
                 green = int(cubes[i-1])
-        if red > RED_LIM or green > GREEN_LIM or blue > BLUE_LIM:
-            impossible_game = True
-        #print(red,green,blue)
-        #assert False
-    if impossible_game == False:
-        summe += iden
+                if green > curr_green:
+                    curr_green = green
+    #     if red > RED_LIM or green > GREEN_LIM or blue > BLUE_LIM:
+    #         impossible_game = True
+    #     #print(red,green,blue)
+    #     #assert False
+    # if impossible_game == False:
+    #     summe += iden
+    power = curr_blue*curr_green*curr_red
+    summe+= power
+    
 print(summe)
