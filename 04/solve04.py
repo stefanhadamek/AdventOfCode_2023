415e57daa8d483c10e8ad0ed998f286150f5fc0c
root <stefan.hadamek@fau.de>
2023-12-05 19:00:01 +0100

    Thomas is smart

--- 04/solve04.py
+++ 04/solve04.py
@@ -41,3 +41,25 @@ for l in lines:
     for card in our_cards:
         if card in winning_cards:
             matches+=1
+
+number_of_cards = [1]*220
+
+for i,l in enumerate(lines):
+    l=l[:-1]
+    ids,cards = l.split(":")
+    winning_cards,our_cards = cards.split("|")
+    #assert False
+    winning_cards = winning_cards.split(" ")
+    winning_cards =[item for item in winning_cards if item != '']
+    our_cards = our_cards.split(" ")
+    our_cards =[item for item in our_cards if item != '']
+    print(winning_cards,our_cards)
+    matches = 0 
+    for card in our_cards:
+        if card in winning_cards:
+            matches+=1
+    
+    for j in range(matches):
+        number_of_cards[i+j+1] += number_of_cards[i]
+
+print(sum(number_of_cards))
